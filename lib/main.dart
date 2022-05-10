6fd3cbaeb441897bf878fabbb5b45724ae854cf6
Kyle Morton <kylemortondevelopment@gmail.com>
2022-05-10 14:31:15 -0500

    adding interactivity

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
 
   @override
@@ -54,10 +55,24 @@ class _RandomWordsState extends State<RandomWords> {
             _suggestions.addAll(generateWordPairs().take(10));
           }
 
+          final alreadySaved = _saved.contains(_suggestions[index]);
+
           return ListTile(
             title: Text(_suggestions[index].asPascalCase, style: _biggerFont),
+            trailing: Icon(
+                alreadySaved ? Icons.favorite : Icons.favorite_border,
+                color: alreadySaved ? Colors.red : null,
+                semanticLabel: alreadySaved ? 'Remove from saved' : 'Save'),
+            onTap: () {
+              setState(() {
+                if (alreadySaved) {
+                  _saved.remove(_suggestions[index]);
+                } else {
+                  _saved.add(_suggestions[index]);
+                }
+              });
+            },
           );
-          return Text(_suggestions[index].asPascalCase);
         });
   }
 }
